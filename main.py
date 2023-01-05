7c85d4f28d36c927eb90773df2739d5d2b52e642
Mahmut P <109034196+C0derByM4H6301@users.noreply.github.com>
2023-01-05 18:39:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-import youtube_dl
-
-options = {
-    'format': 'bestaudio/best',
-    'outtmpl': '%(title)s.%(ext)s',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }]
-}
-
-
-url = 'https://www.youtube.com/watch?v=Vu1pOXXGIso'
-
-with youtube_dl.YoutubeDL(options) as ydl:
-    ydl.download([url])
+import pygame
+import time
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load("pusu.mp3")
+pygame.mixer.music.play()
+#time.sleep(240)
+#pygame.mixer.music.stop()
